3cbad742ca5d4182274b7477ef691faacde82710
regwhitton <reg.whitton@hatcat.co.uk>
2020-07-21 17:26:16 +0100

    Distance calculator component.

---
+++ src/main/java/regwhitton/dwptest/service/londonusers/Location.java
@@ -0,0 +1,11 @@
+package regwhitton.dwptest.service.londonusers;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+public class Location {
+    Double latitude;
+    Double longitude;
+}
